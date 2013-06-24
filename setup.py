00cf0860331ff4ec7c8cc6905a1fb3bae478f9a5
Haak Saxberg <haak.erling@gmail.com>
2013-06-23 20:47:30 -0700

    gitignore and setup for pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='utils',
+    version='0.1',
+    description='Python Distribution Utilities',
+    long_description=open("README.txt").read(),
+    author='Haak Saxberg',
+    author_email='haak.erling@gmail.com',
+    url='http://github.com/haaksmash/pyutils',
+    packages=find_packages(),
+)
