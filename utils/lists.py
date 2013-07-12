2fd84500c2812cbf76173b2c4d712139dcec16e4
Haak Saxberg <haak.erling@gmail.com>
2013-07-11 21:14:18 -0700

    refinements to unlist

--- utils/lists.py
+++ utils/lists.py
@@ -4,5 +4,9 @@
 def unlist(list_thing, complain=True):
     if complain and len(list_thing) > 1:
         raise ValueError("More than one element in {}".format(list_thing))
+    elif len(list_thing) == 1:
+        return list_thing[0]
 
-    return list_thing[0]
+    if complain:
+        raise ValueError("Nothing in {}".format(list_thing))
+    return None
