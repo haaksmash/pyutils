e5e872c10d8da5ddc9cacd710b01e7da57b2e7cc
Haak Saxberg <haak.erling@gmail.com>
2013-08-09 02:34:23 -0700

    a start

--- utils/enum.py
+++ utils/enum.py
@@ -10,17 +10,17 @@ class enum(object):
         self.__enum_items = {}
 
         counter = 0
-        for arg, val in kwargs.iteritems():
+        for name, val in kwargs.iteritems():
             if val is None:
                 val = counter
                 counter += 1
             elif isinstance(val, int):
                 counter = val + 1
 
-            self.__enum_items[arg] = val
+            self.__enum_items[name] = EnumItem(self, name, val)
 
-        for val, arg in enumerate(args, start=counter):
-            self.__enum_items[arg] = val
+        for val, name in enumerate(args, start=counter):
+            self.__enum_items[name] = EnumItem(self, name, val)
 
     def __getattr__(self, attr_name):
         if attr_name in self.__enum_items:
@@ -41,13 +41,63 @@ class enum(object):
             yield k, v
 
     def __repr__(self):
-        start = "<{}: ".format(self.__class__.__name__)
-        values = []
-        for k, v in self:
-            values += ["{}={}".format(k, v)]
-        end = ">"
-        return start + ", ".join(values) + end
+        return "<{}: {}>".format(self.__class__.__name__, self.__enum_items.values())
 
 
 class EnumItem(object):
-    pass
+
+    def __init__(self, parent, name, value):
+        self.__parent = parent
+        self.__name = name
+        self.__value = value
+
+    def __repr__(self):
+        return "<{}: {} [{}]>".format(self.__class__.__name__, self.name, self.value)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            if self.parent != other.parent:
+                raise ValueError("Can't compare EnumItems from different enums")
+            return self.value == other.value
+
+        return self.value == other
+
+    @property
+    def value(self):
+        return self.__value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def parent(self):
+        return self.__parent
+
+
+class _EnumMeta(type):
+    def __new__(cls, name, bases, attr_dict):
+        print bases
+
+        options = attr_dict.pop('Options', object)
+
+        for attr_name, attr_value in attr_dict.items():
+            if attr_name.startswith('__'):
+                continue
+
+            if getattr(options, 'force_uppercase', False):
+                attr_name = attr_name.upper()
+            attr_dict[attr_name] = EnumItem(name, attr_name, attr_value)
+
+        if getattr(options, "frozen", False):
+            attr_dict['__frozen__'] = True
+
+        return super(_EnumMeta, cls).__new__(cls, name, bases, attr_dict)
+
+    def __setattr__(self, name, val):
+        if getattr(self, "__frozen__"):
+            raise TypeError("can't set attributes on a frozen enum")
+
+
+class Enum(object):
+    __metaclass__ = _EnumMeta
