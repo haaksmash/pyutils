1b7c5744a939c69d5f89c008079a221bd0942f3e
Haak Saxberg <haak.erling@gmail.com>
2013-08-17 13:22:15 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='utils',
-    version='0.3',
+    version='0.4',
     description='Python Distribution Utilities',
     long_description=open("README.txt").read(),
     author='Haak Saxberg',
