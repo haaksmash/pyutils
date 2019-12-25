111e1bbdd27578caf77109af0bcae27bc2f2d51a
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2019-12-25 17:52:09 +0530

    Fix DeprecationWarning regarding collections.abc import

--- utils/math.py
+++ utils/math.py
@@ -1,4 +1,7 @@
-import collections
+try:
+    from collections.abc import Iterable
+except ImportError:
+    from collections import Iterable
 import operator
 
 # py3 doesn't include reduce as a builtin
@@ -10,7 +13,7 @@ except NameError:
 
 def product(sequence, initial=1):
     """like the built-in sum, but for multiplication."""
-    if not isinstance(sequence, collections.Iterable):
+    if not isinstance(sequence, Iterable):
         raise TypeError("'{}' object is not iterable".format(type(sequence).__name__))
 
     return reduce(operator.mul, sequence, initial)
